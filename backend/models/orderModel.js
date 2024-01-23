42e6e11d2f2ad99d6e7b0178d465cc5f69582098
akelpie <jdmasopeh@Outlook.com>
2024-01-22 22:48:34 -0500

    seeding sample data

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const orderSchema = moongoose.Schema({
+const orderSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
