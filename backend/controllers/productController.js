4e0ea36aee949a69ddb5151b1feebd1bc39fff63
akelpie <jdmasopeh@Outlook.com>
2024-01-30 20:10:22 -0500

    User Routes & Controller, ex. login, logout, profile.. works on postman

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,6 +8,7 @@ const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
+
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
