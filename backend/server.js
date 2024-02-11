a470ebdafdfcdc9c0dca31461c3d0c7b864fb7de
akelpie <jdmasopeh@Outlook.com>
2024-02-11 14:08:04 -0500

    Order Routes and Contoller. Also updated on Postman.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
-import connectDB from './config/db.js';
-import {notFound, errorHandler } from './middleware/errorMiddleware.js'; // import error handlers
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"; // import error handlers
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -15,20 +16,20 @@ const app = express();
 
 // Body parser middle layer
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // cookie parse middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.use(notFound); 
+app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`)); 
+app.listen(port, () => console.log(`Server running on port ${port}`));
