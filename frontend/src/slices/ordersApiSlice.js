a29e5cca8d2199f7c31a7f4cddaa0f259f1700a7
akelpie <jdmasopeh@Outlook.com>
2024-02-12 20:54:41 -0500

    Creating An Order.

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,11 +1,11 @@
 import { apiSlice } from './apiSlice'; 
-import { OREDRS_URL  } from "./constants";
+import { ORDERS_URL  } from "../constants";
 
 export const ordersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         createOrder: builder.mutation({
             query: (order) => ({
-                url: OREDRS_URL,
+                url: ORDERS_URL,
                 method: 'POST',
                 body: { ...order},
             }),
