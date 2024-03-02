d41f708c757ad34f5c10eb9b5ded75ac27ac4cdc
Jasmine Masopeh <jdmasopeh@outlook.com>
2024-03-01 21:01:25 -0500

    paginate component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 // import products from '../products';
@@ -29,6 +30,11 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            keyword=""
+          />
         </>
       )}
     </>
