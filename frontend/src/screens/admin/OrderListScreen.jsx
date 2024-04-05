2836a4070979a6a5f7e5db507ab24eb295ffa9b2
Jasmine Masopeh <jdmasopeh@outlook.com>
2024-04-04 22:59:12 -0400

    Prefer using an optional chain expression instead, as it's more concise and easier to read orderlistscreen.jsx

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -49,7 +49,7 @@ const OrderListScreen = () => {
               return (
                 <tr key={order._id}>
                   <td>{order._id}</td>
-                  <td>{order.user && order.user.name}</td>
+                  <td>{order.user?.name}</td>
                   <td>{order.createdAt.substring(0, 10)}</td>
                   <td>${order.totalPrice}</td>
                   <td>{isPaid}</td>
