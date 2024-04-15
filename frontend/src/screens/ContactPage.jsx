66ac2f900fe8650775c996f20574c51df6ffb4d0
RaulGonzal <Raulgonzalc@yahoo.com>
2024-04-15 02:03:16 -0400

    contact us page

--- frontend/src/screens/ContactPage.jsx
+++ frontend/src/screens/ContactPage.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import {Form, Button} from "react-bootstrap";
+import emailjs from '@emailjs/browser';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const ContactPage = () => {
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  const form = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!subject || !message) {
+      setError("Please fill out all fields.");
+      return;
+    }
+
+    emailjs.sendForm('service_5i0nwwh', 'template_bmc6r7k', form.current,
+      {
+        publicKey: 'hk70Rcy-_p74TUEfo',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setSubject("")
+          setMessage("")
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      )
+  }
+
   return (
-    <div>ContactPage</div>
+    <>
+      <Form ref={form} onSubmit={submitHandler}>
+        <Form.Group className="my-2" controlId="subject">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control
+            type="text"
+            name="subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="message">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            type="text"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" value="send" variant="primary">
+          Submit
+        </Button>
+      </Form>
+    </>
   )
 }
 
