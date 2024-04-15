57ef4ff14c8c6310ff3ac3e7ba147a1c930afd38
Jasmine Masopeh <jdmasopeh@outlook.com>
2024-04-15 13:56:58 -0400

    Have three social-media sites

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -11,8 +11,17 @@ import Meta from '../components/Meta';
 import { useGetProductDetailsQuery, useCreateReviewMutation } from "../slices/productsApiSlice";
 import { addToCart } from '../slices/cartSlice';
 import { moveToSave } from '../slices/moveSlice';
+import {
+  FacebookShareButton, 
+  FacebookIcon, 
+  LinkedinShareButton,
+  LinkedinIcon,
+  RedditShareButton,
+  RedditIcon,
+} from 'react-share';
 
 const ProductScreen = () => {
+    const shareUrl = window.location.href
     const { id: productId } = useParams();
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -107,23 +116,47 @@ const ProductScreen = () => {
         return formOrMessage;
       };
 
-      return (
-        <>
-          <Link className="btn btn-light my-3" to="/">
-            Go Back
-          </Link>
-          {isLoading ? (
-            <Loader />
-          ) : error ? (
-            <Message variant='danger'>{error?.data?.message || error.error}</Message>
-          ) : (
-            <>
-              <Meta title={product.name} description={product.description} />
-              <Row>
-                <Col md={5}>
-                  <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col md={4}>
+    return (
+      <>
+        <Link className="btn btn-light my-3" to="/">
+          Go Back
+        </Link>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error?.data?.message || error.error}</Message>
+        ) : (
+          <>
+            <Meta title={product.name}
+                  description={product.description} />
+            <FacebookShareButton url={shareUrl}>
+              <FacebookIcon size={40}/>
+            </FacebookShareButton>
+            <LinkedinShareButton url={shareUrl} title={"this is in there"}>
+              <LinkedinIcon size={40}/>
+            </LinkedinShareButton>
+            <RedditShareButton url={shareUrl}>
+              <RedditIcon size={40}/>
+            </RedditShareButton>
+
+            <Row>
+              <Col md={5}>
+                <Image src={product.image} alt={product.name} fluid />
+              </Col>
+              <Col md={4}>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                  </ListGroup.Item>
+                  <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                  <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+                </ListGroup>
+              </Col>
+              <Col md={3}>
+                <Card>
                   <ListGroup variant='flush'>
                     <ListGroup.Item>
                       <h3>{product.name}</h3>
