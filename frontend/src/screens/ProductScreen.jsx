0a81852ee833dee3b1386c6640e642eb5692c3b9
Jasmine Masopeh <jdmasopeh@outlook.com>
2024-04-26 12:26:39 -0400

    added twitter meta information to see if I get more details

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -141,7 +141,7 @@ const ProductScreen = () => {
             <meta property="og:title" content={product.name} />
             <meta property="og:description" content={product.description} />
             <meta property="og:url" content={shareUrl} />
-            
+
             <TelegramShareButton url={shareUrl} title={product.description }>
               <TelegramIcon size={40}/>
             </TelegramShareButton>
@@ -152,6 +152,10 @@ const ProductScreen = () => {
               <WhatsappIcon size={40}/>
             </WhatsappShareButton>
 
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="Your Product Title" />
+            <meta name="twitter:description" content={product.description} />
+            <meta name="twitter:image" content={product.image}/>
             <Row>
                 <Col md={5}>
                   <Image src={product.image} alt={product.name} fluid />
